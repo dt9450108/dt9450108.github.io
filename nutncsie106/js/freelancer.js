c8091b1a291023fe7e1f4a1a72188ee0747d66b8
dt9450108 <dt9450108@gmail.com>
2016-11-20 02:10:05 +0800

    NUTN CSIE 106 Projects

---
+++ nutncsie106/js/freelancer.js
@@ -0,0 +1,153 @@
+// Freelancer Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    // Floating label headings for the contact form
+    $(function() {
+        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+        }).on("focus", ".floating-label-form-group", function() {
+            $(this).addClass("floating-label-form-group-with-focus");
+        }).on("blur", ".floating-label-form-group", function() {
+            $(this).removeClass("floating-label-form-group-with-focus");
+        });
+    });
+
+    // Add portfolio contents
+    initPortfolio();
+})(jQuery); // End of use strict
+
+function initPortfolio() {
+    for (var i = 0; i < Projects.length; i++) {
+        $('#portfolio-content').append(
+            '<div class="col-sm-4 portfolio-item">' +
+            '<a class="portfolio-link" onclick="ShowProjectDialog(' + i + ');"><div class="caption">' +
+            '<div class="caption-content"><i class="fa fa-search-plus fa-3x"></i></div></div>' +
+            '<img src="img/portfolio/' + (Projects[i].img == null ? "no_photo.jpg" : Projects[i].id + '/' + Projects[i].img[0] + '.jpg') + '" class="img-responsive" alt="' + Projects[i].title_zhtw + '"></a>' +
+            '<div class="portfolio-item-info"><div><p class="portfolio-item-info-header">' + Projects[i].title_zhtw +
+            '</p></div><div><p class="portfolio-item-info-header">學生：</p>' +
+            '<p class="portfolio-item-info-content">' + Projects[i].student + '</p></div><div><p class="portfolio-item-info-header">指導教授：</p>' +
+            '<p class="portfolio-item-info-content">' + Projects[i].teacher + '</p></div></div></div>'
+        );
+
+        $("#presentation-list").append(presentationItem(Projects[i]));
+        if (i == 15) {
+            $("#presentation-list").append('<tr><th scope="row"></th><td>15:00 - 15:20</td><td>中場休息</td><td></td><td></td></tr>');
+        }
+    }
+}
+
+function ShowProjectDialog(id) {
+    var proj = Projects[id],
+        html = ProjectItem(proj);
+    $('#modal-body').html(html);
+    $('#portfolioModal').modal('show');
+    if (proj.img != null && proj.img.length > 1) {
+        jssorSlidesInit();
+    }
+}
+
+function ProjectItem(proj) {
+    var html = '<h2>' + proj.title_zhtw + '</h2>' +
+        '<hr class="star-primary">';
+
+    if (proj.img != null && proj.img.length > 1) {
+        html += jssorHtml(proj.id, proj.img);
+    } else {
+        html += '<img src="img/portfolio/' + (proj.img == null ? "no_photo.jpg" : proj.id + '/' + proj.img[0] + '.jpg') + '" class="img-responsive img-centered" alt="">';
+    }
+
+    html += '<br><p class="paragraph-indent paragraph-align">' + proj.introduction + '</p>' +
+        '<ul class="list-inline item-details">' +
+        '<li><p>學生:<strong>' + proj.student + '</strong></p></li>' +
+        '<li><p>指導教授:<strong>' + proj.teacher + '</strong></p></li>' +
+        '<li><p>海報:<strong><a href="' + (proj.poster == null ? "#" : proj.poster) + '" target="_blank">觀看海報</a></strong></p></li></ul>' +
+        '<button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> 關閉</button>';
+
+    return html;
+}
+
+function jssorSlidesInit() {
+    var jssor_1_options = {
+        $AutoPlay: true,
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        },
+        $ThumbnailNavigatorOptions: {
+            $Class: $JssorThumbnailNavigator$,
+            $Cols: 9,
+            $SpacingX: 3,
+            $SpacingY: 3,
+            $Align: 260,
+            $ChanceToShow: 1
+        }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor-content", jssor_1_options);
+
+    /*responsive code begin*/
+    /*you can remove responsive code if you don't want the slider scales while window resizing*/
+    var ScaleSlider = function() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 800);
+            jssor_1_slider.$ScaleWidth(refSize);
+        } else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    };
+
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    /*responsive code end*/
+}
+
+function jssorHtml(id, img) {
+    var html = '<div id="jssor-content" class="jssor-content-page"><div data-u="loading" class="jssor-content-loading"><div class="jssor-content-loading-background"></div>' +
+        '<div class="jssor-content-loading-image"></div></div><div data-u="slides" class="jssor-content-slides">' +
+        '<div data-p="112.50"><img data-u="image" height="300" src="img/portfolio/' + id + '/' + img[0] + '.jpg" /><img data-u="thumb" height="30" src="img/portfolio/' + id + '/' + img[0] + '.jpg" /></div>';
+
+    for (var i = 1; i < img.length; i++) {
+        html += '<div data-p="112.50" style="display: none;"><img data-u="image" height="300" src="img/portfolio/' + id + '/' + img[i] + '.jpg" /><img data-u="thumb" height="30" src="img/portfolio/' + id + '/thumb_' + img[i] + '.jpg" /></div>';
+    }
+
+    html += '</div><div data-u="thumbnavigator" class="jssor-content-thumbnavigator jssort03" data-autocenter="1"><div class="jssor-content-thumbnavigator-background"></div>' +
+        '<div data-u="slides" class="jssor-content-thumbnavigator-slides"><div data-u="prototype" class="p"><div class="w"><div data-u="thumbnailtemplate" class="t"></div>' +
+        '</div><div class="c"></div></div></div></div><span data-u="arrowleft" class="jssor-content-page-arrowleft jssora02l" data-autocenter="2"></span>' +
+        '<span data-u="arrowright" class="jssor-content-page-arrowright jssora02r" data-autocenter="2"></span></div>';
+    return html;
+}
+
+function presentationItem(proj) {
+    return '<tr><th scope="row">' + proj.id + '</th><td>' + proj.presentation_time + '</td><td>' + proj.title_zhtw + '</td><td>' + proj.teacher + '</td><td>' + proj.student + '</td></tr>';
+}
---
+++ nutncsie106/js/projects_data.js
@@ -0,0 +1,253 @@
+/*
+1 - 黃詩豪、袁恆朗、洗鈺淇
+2 - 陳奕廷、臧芷藝
+3 - 趙冠銘、吳述忱
+4 - 許庭瑋
+5 - 李易璋、胡泰翔
+6 - 張辰安
+7 - 吳品辰、洪健哲
+8 - 陳怡如、張君意、陳采琦
+9 - 黎桂如
+10 - 周彥良、林靖恒
+11 - 楊孟洵
+12 - 吳承翰、李亞修
+13 - 張博凱
+14 - 賴郁文、劉恩銘
+15 - 方清鋒
+16 - 李宗霖
+17 - 鍾丞斌
+18 - 劉軒佑
+19 - 李珮甄、蕭瑀
+20 - 賴靖融
+21 - 陳品安、柯品佑
+*/
+
+var Projects = [{
+    "id": 1,
+    "title_zhtw": "健身房運動行為分析與會員管理系統",
+    "title_en": "Fitness Behavior Analysis and Gym Member Management System",
+    "introduction": "近年來，國人飲食及生活型態的改變，促進了運動風氣的盛行，規律的運動可以有效增進生理機能，且對於現代人在極高的工作及課業壓力下，運動也成了一個調節心理情緒和建立自信的一種方式，而在都市社會中，健身房提供了現代人在室內舒適的健身機會，一系列的規劃，從走入健身房，健身項目的選擇，教練的引導，汗流浹背的享受各式的健身課程，結束後於淋浴間進行沖澡，迅速的完成整套流程，因應此全新的健康生活概念，我們試著結合健身房會員固有的行為模式和室內定位（Indoor Location Tracking），來搜集更多健身資訊，使健身房的使用者、教練以及健身房管理者更能掌握在健身房內的動態，更能進一步分析會員行為模式，而創造更多效益。",
+    "student": "黃詩豪、袁恆朗、洗鈺淇",
+    "teacher": "林朝興",
+    "poster_url": null,
+    "img": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    "presentation_time": "13:00 - 13:10"
+}, {
+    "id": 2,
+    "title_zhtw": "多重資訊來源之基因表現群聚分析整合系統開發",
+    "title_en": "A Platform for Cluster Analysis  of Gene Expression Data with Multiple Information Sources",
+    "introduction": "設計與開發整合不同基因資訊來源的基因表現分群分析軟體平台系統，實現以拔靴法自動決定不同基因資訊來源的權重分配(例如將基因資料表現相似度及基因語意相似度自動做權重分配)，並提供過去常用的各種不同分群法、不同距離測度法以及不同語意相似度計算方法的選項，將所有能分析資料群聚的方法都結合在一起，希望能方便使用者使用於分析基因表現資料的分群",
+    "student": "陳奕廷、臧芷藝",
+    "teacher": "陳榮銘",
+    "poster_url": null,
+    "img": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    "presentation_time": "13:10 - 13:20"
+}, {
+    "id": 3,
+    "title_zhtw": "使用擴增實境製作電子賀卡",
+    "title_en": "",
+    "introduction": "以往卡片通常以節慶主題或生日、慶祝等時機特別表達心意，並僅有文字方面的傳達。使用AR擴增實境達成包裝資訊的目的，此AR賀卡外觀只有一張圖片，欲得知內容則需要使用對應APP掃測圖片，藉以呈現隱藏在其中的虛擬資訊（立體模型、影片）。",
+    "student": "趙冠銘、吳述忱",
+    "teacher": "高啟洲",
+    "poster_url": null,
+    "img": [1, 2],
+    "presentation_time": "13:20 - 13:30"
+}, {
+    "id": 4,
+    "title_zhtw": "植基於手部移動之三軸無人機操控",
+    "title_en": "",
+    "introduction": "據美國聯邦航空總署FAA預測，無人機飛行器將呈現急遽成長，商業使用的無人航空器將有逐步增長趨勢，除可用於空中拍攝應用外，進階可拓展貨物酬載、環境探測、高危高空作業等應用情境。除了商業的發展應用，它正朝著更多面向去擴展來帶給人們更多生活上的福祉。",
+    "student": "許庭瑋",
+    "teacher": "李建樹",
+    "poster_url": null,
+    "img": [1, 2],
+    "presentation_time": "13:30 - 13:40"
+}, {
+    "id": 5,
+    "title_zhtw": "AR擴增實境-第三人稱視角射擊遊戲",
+    "title_en": "",
+    "introduction": "因應潮流，運用AR擴增實境的方式去開發一個具遊戲性手持式行動裝置上的應用程式，除了去開發一個屬於自己創作出來的作品，也可以透過此開發，接觸自己從未碰觸過的領域、強化自己從未增進的能力。以及透過作品的發表來知曉目前使用者們對於這項技術於遊戲上發展的需求與願望和了解我們的不足之處。",
+    "student": "李易璋、胡泰翔",
+    "teacher": "高啟洲",
+    "poster_url": null,
+    "img": [1, 2, 3, 4, 5, 6],
+    "presentation_time": "13:40 - 13:50"
+}, {
+    "id": 6,
+    "title_zhtw": "股票查詢系統",
+    "title_en": "",
+    "introduction": "可以查詢股票的系統。",
+    "student": "張辰安",
+    "teacher": "楊文霖",
+    "poster_url": null,
+    "img": [1, 2],
+    "presentation_time": "13:50 - 14:00"
+}, {
+    "id": 7,
+    "title_zhtw": "智慧公車舒適度之研究",
+    "title_en": "",
+    "introduction": "隨著交通運輸的快速發展，搭乘大眾交通工具的人越來越多，但是越來越普及的交通工具使得乘客們常常需要在尖峰時刻與許多人擠在一起，而且固定的班次常常讓人一旦錯過就必需再等待下一班，浪費了民眾的時間以及無法確保搭乘大眾運輸工具時的舒適度，所以我們思考如何運用科技，能讓人們在使用及搭乘上更加便利、更加舒適。我們的想法是透過Bluetooth Low Energy(BLE)，將公車上的搭乘資訊，像乘載人數、車內人數分布以及公車行駛安定度等，經過計算後將整體舒適度傳送並顯示至使用者手機上，讓使用者能夠知道目前這班車的搭乘舒適度，也可以將資訊傳送至公車營運部門，告知目前乘載人數的狀況並加派班次等。另外，還可以透過手機連接Wi-Fi，讓使用者可以接收各站點的旅遊觀光資訊等，致力於打造人車互動的物聯網環境和公車的智慧化，未來希望能與市政府合作，共同打造智慧化都市。",
+    "student": "吳品辰、洪健哲",
+    "teacher": "陳宗禧",
+    "poster_url": null,
+    "img": [1, 2, 3, 4, 5],
+    "presentation_time": "14:00 - 14:10"
+}, {
+    "id": 8,
+    "title_zhtw": "智慧型數位打卡系統",
+    "title_en": "Intelligent Digital Attendance Recording System",
+    "introduction": "我們使用無線射頻辨識系統（Radio Frequency Identification，以下簡稱 RFID）和光學指紋識別辨識模組（以下簡稱指紋模組）取代傳統的打卡鐘，降低紙張的耗費並減少人力成本，讓系統自動記錄出勤狀況，透過 WiFi晶片 ESP8266來傳送驗證資料給電腦資料庫並加以儲存，同時使管理者易於查詢。",
+    "student": "陳怡如、張君意、陳采琦",
+    "teacher": "林朝興",
+    "poster_url": null,
+    "img": [1, 2, 3, 4, 5],
+    "presentation_time": "14:10 - 14:20"
+}, {
+    "id": 9,
+    "title_zhtw": "運用資料探勘技術於專利文件分析與專利地圖製作",
+    "title_en": "Data Mining applied on Patent Analysis and Patent Map Visualization",
+    "introduction": "隨著知識經濟的蓬勃發展，專利技術的取得已經成為企業維護自身利益之最佳工具，而這些專利技術目前已經形成龐大的專利資料庫。專利資料庫除了可以讓我們了解他人的技術範圍，以避免侵權外，藉由分析專利資料庫更可以幫助我們預測競爭對手的技術發展動向與研發趨勢。而為了有效率的進行專利技術分析，專利地圖是常用的輔助工具，意即利用圖表幫助技術研發者快速了解目前技術發展之方向。但在建構專利地圖的過程當中，若使用人工方式進行分析與分類，會消耗大量人力與時間。因此，本研究希望能結合文字探勘與文件分類技術，實作一檢索分析工具，運用網路爬蟲系統(Web Crawler)自動擷取所需資料，運用文件分析技術進行分析與分類，最後以相關專利地圖呈現，幫助使用者能更快取得需要的資訊，減少過程中投入的人力與時間。",
+    "student": "黎桂如",
+    "teacher": "錢炳全",
+    "poster_url": null,
+    "img": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    "presentation_time": "14:20 - 14:30"
+}, {
+    "id": 10,
+    "title_zhtw": "按圖索蹟 App－台南市古蹟檢索",
+    "title_en": "",
+    "introduction": "本計畫想要在行動裝置上以不使用網路的方式來做出能夠使觀光客了解台南市古蹟的行動應用程式(App)，以達到便利觀光客認識與了解台南市古蹟的目的。",
+    "student": "周彥良、林靖恒",
+    "teacher": "李建樹",
+    "poster_url": null,
+    "img": [1, 2],
+    "presentation_time": "14:30 - 14:40"
+}, {
+    "id": 11,
+    "title_zhtw": "數位家庭手機行控系統",
+    "title_en": "",
+    "introduction": "以Android APP進行智慧家庭監控、管理、紀錄，透過簡易的使用者介面，讓使用者能方便操控家中電源設備及裝置。",
+    "student": "楊孟洵",
+    "teacher": "高啟洲",
+    "poster_url": null,
+    "img": [1, 2, 3, 4, 5, 6, 7, 8],
+    "presentation_time": "14:40 - 14:50"
+}, {
+    "id": 12,
+    "title_zhtw": "實況串流APP",
+    "title_en": "",
+    "introduction": "實況串流",
+    "student": "吳承翰、李亞修",
+    "teacher": "楊文霖",
+    "poster_url": null,
+    "img": [1, 2, 3],
+    "presentation_time": "14:50 - 15:00"
+}, {
+    "id": 13,
+    "title_zhtw": "修修臉App－影像修圖鑑識",
+    "title_en": "",
+    "introduction": "製作「修修臉」App，用來檢測照片是否被修圖過，首先，匯入一張想比對的照片或圖，透過分析雜訊，來判斷該影像是否有經過修圖。",
+    "student": "張博凱",
+    "teacher": "李建樹",
+    "poster_url": null,
+    "img": [1],
+    "presentation_time": "15:20 - 15:30"
+}, {
+    "id": 14,
+    "title_zhtw": "以單眼攝影機即時進行同步定位與地圖構建",
+    "title_en": "Real-Time Simultaneous Localisation and Mapping with a Single Camera",
+    "introduction": "將MonoSLAM技術應用於機器人或一般鏡頭裝置的即時3D定位與地圖建構，此方法的核心在機器人或一般鏡頭裝置平滑快速移動經過未知的場景時，透過鏡頭觀察到自然景物經由隨機標定後，持續即時建立不連續的3D軌跡地圖：這其中包含主動地圖建構及測量，能用於和解決裝置初始化位置及方向估算。",
+    "student": "賴郁文、劉恩銘",
+    "teacher": "朱明毅",
+    "poster_url": null,
+    "img": [1, 2],
+    "presentation_time": "15:30 - 15:40"
+}, {
+    "id": 15,
+    "title_zhtw": "手勢解鎖",
+    "title_en": "Mobile Phone Unlock Using Trajectory",
+    "introduction": "從早期的數字鎖開始，漸漸有了圖形鎖，但是這些鎖經過了多次的嘗試，然有可能會被外人解開。近年來更發展了指紋鎖，雖然此鎖辨識複雜，唯有使用者可以進行解鎖，可是仍有些情況無法正常使用，例如同一位使用者設定食指為鑰匙，拇指便無法正常辨識;下雨天或洗完手，手部潮濕時進行解鎖仍會有誤判無法解鎖的情況。因此，我們研究設計一道軌跡鎖，盡可能排除上述的外在因素，建立一道可供使用者操作且辨識度高的密碼鎖。",
+    "student": "方清鋒",
+    "teacher": "李建樹",
+    "poster_url": null,
+    "img": [1],
+    "presentation_time": "15:40 - 15:50"
+}, {
+    "id": 16,
+    "title_zhtw": "智慧型個人行動輔具核心技術研究",
+    "title_en": "Research on the Core Technology of Intelligent Personal Mobility Aids",
+    "introduction": "由於醫療技術的進步與人口結構的改變，台灣已逐漸邁入高齡化的社會，在未來，如何讓高齡化族群能夠藉由科技的協助，而享有獨立自主的生活，已成為一個重要的研究議題。本專題以機器人作業系統ROS(Robot Operating System)為系統開發之環境，已分散式系統架構研究與實作機器人遠端搖控、地圖建構、定位、自動導航及人機介面等核心技術，並將其導入電動輪椅與電動代步車等相關類型之個人行動輔具，發展醫療照護所需之智慧型行動輔具，期望能進而改善高齡化族群之生活品質。",
+    "student": "李宗霖",
+    "teacher": "朱明毅",
+    "poster_url": null,
+    "img": [1],
+    "presentation_time": "15:50 - 16:00"
+}, {
+    "id": 17,
+    "title_zhtw": "Arduino自走車",
+    "title_en": "",
+    "introduction": "Arduino自走車",
+    "student": "鍾丞斌",
+    "teacher": "高啟洲",
+    "poster_url": null,
+    "img": [1],
+    "presentation_time": "16:00 - 16:10"
+}, {
+    "id": 18,
+    "title_zhtw": "結合引文網路之論文推薦系統",
+    "title_en": "",
+    "introduction": "隨著資訊科技與網路的快速發展，學術論文的電子化已經成為趨勢，隨著電子論文的快速傳播，使得電子論文數量呈現爆炸性的成長，造成透過關鍵字搜尋方式會得到許多不同領域的文章，因此一個有效率的搜尋引擎便顯得更為重要。面對龐大的論文資料庫，使用者須逐一檢查論文是否與本身研究主題相關，而推薦系統是被提出的解決方法之一；然而，是否能廣泛收集使用者喜好資訊，以及能否即時呈現相關性高的推薦資訊便是問題所在。<br>本計畫將分析論文間參考及引用的關聯，嘗試建構出論文間的網絡架構，並透過網頁排名以及推薦系統的技術，將其應用在計算論文網絡中各論文的權重和論文之關鍵字出現頻率，進一步設計整合排序方法，透過簡單清楚的使用者介面呈現計算結果，以提供解決推薦資訊缺乏及檢索效率不彰之問題。",
+    "student": "劉軒佑",
+    "teacher": "錢炳全",
+    "poster_url": null,
+    "img": [1],
+    "presentation_time": "16:10 - 16:20"
+}, {
+    "id": 19,
+    "title_zhtw": "利用智慧型手機Wi-Fi群眾外包資料設計與實作一基於興趣點的行動服務系統",
+    "title_en": "",
+    "introduction": "透過Wi-Fi接收訊號強度的變化量與感測器之數值分析使用者移動行為，判斷使用者是否對該處商店感興趣，並且利用群眾外包的方式，藉由分析不同使用者的興趣點序列，將各個商店進行分類，進而了解該商店吸引到之客群。透過比對POI序列及POI的特性預測下一個可能會需要服務的地方，進而提升消費者體驗。",
+    "student": "李珮甄、蕭瑀",
+    "teacher": "陳宗禧",
+    "poster_url": null,
+    "img": [1, 2],
+    "presentation_time": "16:20 - 16:30"
+}, {
+    "id": 20,
+    "title_zhtw": "雙眼視覺系統研究與實作",
+    "title_en": "Research and Implementation of a Binocular Vision System",
+    "introduction": "仿傚人類感知環境的架構與方式，採用由雙攝影機組成的雙眼立體視覺系統來偵測障礙物並計算其距離，進而達成導航。",
+    "student": "賴靖融",
+    "teacher": "朱明毅",
+    "poster_url": null,
+    "img": [1],
+    "presentation_time": "16:30 - 16:40"
+}, {
+    "id": 21,
+    "title_zhtw": "行動裝置定位管理系統",
+    "title_en": "",
+    "introduction": "結合wifi室內定位的相關應用app",
+    "student": "陳品安、柯品佑",
+    "teacher": "楊文霖",
+    "poster_url": null,
+    "img": [1, 2, 3],
+    "presentation_time": "16:40 - 16:50"
+}];
+
+// console.log("id\ttitle_zhtw\ttitle_en\tintroduction\tstudent\tteacher\tposter_url\timg\tpresentation_time");
+// for (var i = 0; i < Projects.length; i++)
+//     console.log(Projects[i].id);
+// for (var i = 0; i < Projects.length; i++)
+//     console.log(Projects[i].title_zhtw);
+// for (var i = 0; i < Projects.length; i++)
+//     console.log(Projects[i].title_en);
+// for (var i = 0; i < Projects.length; i++)
+//     console.log(Projects[i].introduction);
+// for (var i = 0; i < Projects.length; i++)
+//     console.log(Projects[i].student);
+// for (var i = 0; i < Projects.length; i++)
+//     console.log(Projects[i].teacher);
+// for (var i = 0; i < Projects.length; i++)
+//     console.log(Projects[i].img);
+// for (var i = 0; i < Projects.length; i++)
+//     console.log("%s - %s", Projects[i].id, Projects[i].student);
